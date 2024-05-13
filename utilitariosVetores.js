44b0a66d04584ea9dd037832bb81696b09a48587
Wagner <wagner.worst@gmail.com>
2024-05-13 20:37:21 -0300

    Alterado o filtro pra realizar um like em vez de equals

--- utilitariosVetores.js
+++ utilitariosVetores.js
@@ -1,10 +1,10 @@
 export function FiltrarVetor(listaOriginal, valorDeBusca, propriedade) {
     if(propriedade == "title")
     {
-        return listaOriginal.filter((objeto) => objeto.title == valorDeBusca);
+        return listaOriginal.filter((objeto) => objeto.title.includes(valorDeBusca));
     }
     else
     {
-        return listaOriginal.filter((objeto) => objeto.description == valorDeBusca);
+        return listaOriginal.filter((objeto) => objeto.description.includes(valorDeBusca));
     }
 }
